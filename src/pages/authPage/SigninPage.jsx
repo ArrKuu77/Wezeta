16758e02f2acb89493a1707234a24273ce9a970e
ArrKuu77 <aungkungsan@gmail.com>
2026-01-06 22:52:58 -1100

    authGoogleRemove

--- src/pages/authPage/SigninPage.jsx
+++ src/pages/authPage/SigninPage.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import AuthLayout from "../../components/authComponent/AuthLayout";
 import SignInForm from "../../components/authComponent/SignInForm";
-import SignInwithGoogle from "../../components/authComponent/SignInwithGoogle";
+// import SignInwithGoogle from "../../components/authComponent/SignInwithGoogle";
 
 const SigninPage = () => {
   return (
     <AuthLayout>
       <SignInForm />
       <div className="h-[1px] bg-cyan-500  w-full my-3" />
-      <SignInwithGoogle />
+      {/* <SignInwithGoogle /> */}
     </AuthLayout>
   );
 };
